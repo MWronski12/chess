100f202a6a05ae14fe5ddad07c8b65e25edd1d7d
MWronski12 <wronski.mikolaj@gmail.com>
2023-06-06 21:15:28 +0200

    /home/szekler/chess/build/chess

--- main.cpp
+++ main.cpp
@@ -7,13 +7,6 @@
 using namespace std;
 
 int main() {
-    Search s;
-    Board b( "3r4/pR2N3/2pkb3/5p2/8/2B5/qP3PPP/4R1K1 w - - 1 0" );
-    PieceValidMoves g;
-    g.generateValidMoves( b );
-    auto bestMove = s.getBestMove( b, 10, b.sideToMove == WHITE );
-    cout << "Best move: " << bestMove.toString() << endl;
-
     // Gui *gui = new ConsoleGui();
     // gui->start();
     WindowGui gui = WindowGui();
